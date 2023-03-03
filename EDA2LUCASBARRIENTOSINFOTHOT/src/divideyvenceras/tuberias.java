412bf2976e2cee27b64bebaac9ff52edd07b3660
34644 <34644@LAPTOP-6LRTVUQB>
2023-03-03 17:30:54 +0100

    Ejercicio tuberias

--- EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/tuberias.java
+++ EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/tuberias.java
@@ -2,13 +2,25 @@ package divideyvenceras;
 
 public class tuberias {
 	public static void main(String[] args) {
-		int [] a = {100, 100, 100, 30, 30};
+		int [] a = {100, 30, 30, 30, 30};
 		buscarFallos(a);
 	}
 	public static void buscarFallos(int [] a) {
-		
+		if (a[0] == a[a.length-1]) {
+			System.out.println("No hay fugas.");
+		} else {
+			buscarFallos(a, 0, a.length - 1);
+		}
 	}
 	public static void buscarFallos(int [] a, int i, int j) {
-		
+			if (a[i]!=a[j]) {
+				if (i+1 == j) {
+					System.out.println("Fallo entre " + i + "-" + j);
+				} else {
+					int mitad = (i+j)/2;
+					buscarFallos(a, i, mitad);
+					buscarFallos(a, mitad, j);
+				}
+		}
 	}
 }
