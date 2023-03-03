3577908954058861fa6b793322ac1713a7436874
34644 <34644@LAPTOP-6LRTVUQB>
2023-03-03 17:09:44 +0100

    Subida Inicial

---
+++ EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/tuberias.java
@@ -0,0 +1,14 @@
+package divideyvenceras;
+
+public class tuberias {
+	public static void main(String[] args) {
+		int [] a = {100, 100, 100, 30, 30};
+		buscarFallos(a);
+	}
+	public static void buscarFallos(int [] a) {
+		
+	}
+	public static void buscarFallos(int [] a, int i, int j) {
+		
+	}
+}
