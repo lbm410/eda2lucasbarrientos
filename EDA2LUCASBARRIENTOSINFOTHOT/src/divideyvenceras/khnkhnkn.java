3f4e7d9f32c75f6c1b5021c6464c6694627441f5
Lucas Barrientos Muñoz <lbm40@inlumine.ual.es>
2023-03-03 23:33:40 +0100

    aqaaaaaa

---
+++ EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/khnkhnkn.java
@@ -0,0 +1,8 @@
+package divideyvenceras;
+
+public class khnkhnkn {
+	public static void main(String[] args) {
+		System.out.println("Toy en el museo");
+	}
+
+}
--- EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/tuberias.java
+++ EDA2LUCASBARRIENTOSINFOTHOT/src/divideyvenceras/tuberias.java
@@ -2,10 +2,13 @@ package divideyvenceras;
 
 public class tuberias {
 	public static void main(String[] args) {
-		int [] a = {100, 30, 30, 30, 30};
+		int [] a = {};
 		buscarFallos(a);
 	}
 	public static void buscarFallos(int [] a) {
+		if (a.length == 0) {
+			throw new RuntimeException("Que dise su polla, si no hay sensores, que pollas vas a buscar vieo");
+		}
 		if (a[0] == a[a.length-1]) {
 			System.out.println("No hay fugas.");
 		} else {
@@ -13,7 +16,7 @@ public class tuberias {
 		}
 	}
 	public static void buscarFallos(int [] a, int i, int j) {
-			if (a[i]!=a[j]) {
+			if (a[i] != a[j]) {
 				if (i+1 == j) {
 					System.out.println("Fallo entre " + i + "-" + j);
 				} else {
